d4401d32f868862da221f93d4f76a38f6b99db0f
vucong <vucong@vucong2018.vucong2018>
2024-07-28 12:03:46 +0700

    video 7

---
+++ cmd/cli/viper/main.viper.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+type Config struct {
+	Server struct {
+		Port int `mapstructure:"port"`
+	} `mapstructure:"server"`
+	Databases []struct {
+		User		string 	`mapstructure:"user"`
+		Password	string 	`mapstructure:"password"`
+		Host		string 	`mapstructure:"host"`
+	} `mapstructure:"databases"`
+}
+
+func main() {
+	viper := viper.New()
+	viper.AddConfigPath("./config/")
+	viper.SetConfigName("local")
+	viper.SetConfigType("yaml")
+
+	//
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("Failed to read configuration %v", err)
+	}
+	//
+	fmt.Println("Server Port::", viper.GetInt("server.port"))
+
+
+	// config struct
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		fmt.Printf("Unable to decode config %v", err)
+	}
+
+	for _,db := range(config.Databases) {
+		fmt.Printf("database User: %s, username: %s\n", db.User, db.Password)
+	}
+}
